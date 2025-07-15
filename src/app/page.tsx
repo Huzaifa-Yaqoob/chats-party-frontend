c03187c22bf32b14495a25821c5509fedf51df51
Muhammad Huzaifa Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2025-07-15 22:26:47 +0500

    configure prettier and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      Home
+      <Button>Hello</Button>
+    </div>
   );
 }
